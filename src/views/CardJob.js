eef9f8959bc1786fb7b853ab0e06da1431b66ccb
Xavi <xavigallardo@gmail.com>
2019-11-07 14:08:59 +0100

    FIX Applu to job + cancel aplly

--- src/views/CardJob.js
+++ src/views/CardJob.js
@@ -1,125 +1,12 @@
 import React, { Component } from 'react';
 import { withAuth } from '../Context/AuthContext';
-import jobService from '../services/jobService';
 
 class CardJob extends Component {
-  constructor(props) {
-    super(props);
-    // No llames this.setState() aquí!
-    this.state = { job: this.props.job, user: this.props.user };
-    this.handleApplytoJob = this.handleApplytoJob.bind(this);
-    this.handleCancelApplytoJob = this.handleCancelApplytoJob.bind(this);
-  }
-
-  async componentDidMount() {
-    // const { job, user } = this.props;
-    const { job, user } = this.state;
-    try {
-      const isApplicant = await job.applicants.filter(applicant => applicant._id === user._id);
-      console.log('TCL: CardJob -> componentDidMount -> applicant', isApplicant);
-      const applicant = isApplicant.length > 0;
-
-      this.setState({
-        job,
-        user,
-        loading: false,
-        isApplicant,
-        applicant,
-      });
-    } catch (error) {
-      console.log(error);
-      this.setState({
-        loading: false,
-        error: 'Unable to load JOBS',
-      });
-    }
-  }
-
-  handleApplytoJob = async () => {
-    const { job, user } = this.state;
-    console.log('Vamos a aplicar al Trabajo');
-    try {
-      const newAplication = await jobService.applytoJob(job._id, user._id);
-      const isApplicant = await newAplication.job.applicants.filter(applicant => applicant._id === user._id);
-      const applicant = isApplicant.length > 0;
-      // const applicant = await newAplication.job.applicants.includes(user._id);
-      console.log('TCL: CardJob -> handleApplytoJob -> newAplication', newAplication);
-      await this.props.userData();
-      this.setState({
-        job: newAplication.job,
-        user: newAplication.user,
-        loading: false,
-        applicant,
-      });
-    } catch (error) {
-      console.log(error);
-      this.setState({
-        loading: false,
-        error: 'Unable to load JOBS',
-      });
-    }
-  };
-
-  handleCancelApplytoJob = async () => {
-    const { job, user } = this.state;
-    console.log('Vamos a cancelar la aplicación al Trabajo');
-
-    try {
-      const newAplication = await jobService.cancelApplytoJob(job._id, user._id);
-      const isApplicant = await newAplication.job.applicants.filter(applicant => applicant._id === user._id);
-      const applicant = isApplicant.length > 0;
-      // const applicant = await newAplication.job.applicants.includes(user._id);
-      await this.props.userData();
-      this.setState({
-        job: newAplication.job,
-        user: newAplication.user,
-        loading: false,
-        applicant,
-      });
-    } catch (error) {
-      console.log(error);
-      this.setState({
-        loading: false,
-        error: 'Unable to load JOBS',
-      });
-    }
-  };
-
-  componentDidUpdate(prevProps) {
-    console.log('This PROPS: ', this.props.job._id);
-    if (this.props !== prevProps) {
-      this.props.getJob(this.props.job._id);
-    }
-  }
-
   render() {
     const { job, user } = this.props;
-    const { applicant, isApplicant } = this.state;
-    console.log('TCL: CardJob -> render -> job', job);
-    console.log('TCL: CardJob -> render -> user', user);
-    console.log('TCL: CardJob -> render -> applicant', applicant);
-    console.log('TCL: CardJob -> render -> isApplicant', isApplicant);
+
     return (
       <div>
-        <h1>Job Detail:</h1>
-        {job.done && <h3>JOB COMPLETED</h3>}
-        {user.company && <>{job.applicants.length} Applicants</>}
-        {!user.company && (
-          <>
-            {job.applicants.length} Applicants
-            {!applicant ? (
-              <>
-                <p>Want to APPLY?</p>
-                <button onClick={this.handleApplytoJob}>Apply</button>
-              </>
-            ) : (
-              <>
-                <p>You are an applicant!!</p>
-                <button onClick={this.handleCancelApplytoJob}>Cancel Application</button>
-              </>
-            )}
-          </>
-        )}
         <p>
           Title:
           {job.title}
--- src/views/JobDetail.js
+++ src/views/JobDetail.js
@@ -4,28 +4,57 @@ import { withAuth } from '../Context/AuthContext';
 import CardJob from './CardJob';
 
 class JobDetail extends Component {
-  state = {
-    job: {},
-    loading: true,
-    error: undefined,
-  };
+  constructor(props) {
+    super(props);
+    // No llames this.setState() aquí!
+    this.state = {
+      job: this.props.job,
+      user: this.props.user,
+      loading: true,
+      error: undefined,
+    };
+    this.handleApplytoJob = this.handleApplytoJob.bind(this);
+    this.handleCancelApplytoJob = this.handleCancelApplytoJob.bind(this);
+  }
 
   async componentDidMount() {
     const { user } = this.props;
     const {
       params: { id },
     } = this.props.match;
-    this.getJob(id);
-  }
-
-  getJob = async id => {
     try {
       const job = await jobService.jobDetail(id);
-      return job;
-      // this.setState({
-      //   job,
-      //   loading: false,
-      // });
+
+      const isApplicant = await job.job.applicants.filter(applicant => applicant._id === user._id);
+      const applicant = isApplicant.length > 0;
+
+      this.setState({
+        job: job.job,
+        user,
+        loading: false,
+        isApplicant,
+        applicant,
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: 'Unable to load JOBS',
+      });
+    }
+  }
+
+  async getJob(id) {
+    const { user } = this.state;
+    try {
+      const job = await jobService.jobDetail(id);
+      const isApplicant = await job.job.applicants.filter(applicant => applicant._id === user._id);
+      const applicant = isApplicant.length > 0;
+      this.setState({
+        job: job.job,
+        loading: false,
+        applicant,
+      });
     } catch (error) {
       console.log(error);
       this.setState({
@@ -33,8 +62,58 @@ class JobDetail extends Component {
         error: 'Unable to load JOB',
       });
     }
+  }
+
+  handleApplytoJob = async () => {
+    const { job, user } = this.state;
+
+    // console.log('Vamos a aplicar al Trabajo');
+    try {
+      const newAplication = await jobService.applytoJob(job._id, user._id);
+      const isApplicant = await newAplication.job.applicants.filter(applicant => applicant._id === user._id);
+      const applicant = isApplicant.length > 0;
+
+      await this.props.userData();
+      this.setState({
+        job: newAplication.job,
+        user: newAplication.user,
+        loading: false,
+        applicant,
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: 'Unable to load JOBS',
+      });
+    }
   };
-  // const NewJob = this.getJob(id);
+
+  handleCancelApplytoJob = async () => {
+    const { job, user } = this.state;
+
+    // console.log('Vamos a cancelar la aplicación al Trabajo');
+
+    try {
+      const newAplication = await jobService.cancelApplytoJob(job._id, user._id);
+      const isApplicant = await newAplication.job.applicants.filter(applicant => applicant._id === user._id);
+      const applicant = isApplicant.length > 0;
+      // const applicant = await newAplication.job.applicants.includes(user._id);
+      await this.props.userData();
+      this.setState({
+        job: newAplication.job,
+        user: newAplication.user,
+        loading: false,
+        applicant,
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: 'Unable to load JOBS',
+      });
+    }
+  };
 
   componentDidUpdate(prevProps) {
     if (this.props.match.params.id !== prevProps.match.params.id) {
@@ -43,16 +122,11 @@ class JobDetail extends Component {
   }
 
   render() {
-    const {
-      job: { job },
-      loading,
-      error,
-    } = this.state;
+    const { job, user, applicant, isApplicant, loading, error } = this.state;
+
     const {
       params: { id },
     } = this.props.match;
-
-    console.log('TCL: JobDetail -> render -> job', job);
 
     return (
       <div>
@@ -60,8 +134,25 @@ class JobDetail extends Component {
           <>
             {!loading && (
               <>
-                HOLA
-                <CardJob getJob={this.getJob} job={job}></CardJob>
+                <h1>Job Detail:</h1>
+                {job.done && <h3>JOB COMPLETED</h3>}
+                {job.applicants.length} Applicants
+                {!user.company && (
+                  <>
+                    {!applicant ? (
+                      <>
+                        <p>Want to APPLY?</p>
+                        <button onClick={this.handleApplytoJob}>Apply</button>
+                      </>
+                    ) : (
+                      <>
+                        <p>You are an applicant!!</p>
+                        <button onClick={this.handleCancelApplytoJob}>Cancel Application</button>
+                      </>
+                    )}
+                  </>
+                )}
+                <CardJob job={job}></CardJob>
               </>
             )}
           </>
