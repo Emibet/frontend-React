571a9e952e9824ed9004c7c16d5af373bf33138a
Xavi <xavigallardo@gmail.com>
2019-11-19 11:05:38 +0100

    ResumeUpdate.js

--- src/views/ResumeUpdate.js
+++ src/views/ResumeUpdate.js
@@ -26,17 +26,16 @@ const InputSubmit = styled.input`
 const Input = styled.input`
   display: block;
   margin: 0.5em 0;
-  text-align:end;
-  font-size:1em;
-  // margin: 0.5em auto;
-  width: 100%
+  text-align: end;
+  font-size: 1em;
+  /* // margin: 0.5em auto; */
+  width: 100%;
   border: none;
   border-bottom: 1px solid #757575;
   &:focus {
     outline: none;
   }
   &.freelance {
-    
     outline: 1px solid #1e5180;
     display: block;
     width: 5%;
@@ -60,11 +59,6 @@ class NurseUpdate extends Component {
   };
 
   async componentDidMount() {
-    // const {
-    //   match: {
-    //     params: { id },
-    //   },
-    // } = this.props;
     const { user } = this.props; // Original
 
     try {
@@ -96,21 +90,17 @@ class NurseUpdate extends Component {
             ...nurse.resume,
             [name]: value,
           },
-          // [name]: type === 'checked' ? checked : value,
         },
       },
     });
   };
 
   handleChangeBox = event => {
-    console.log('TCL: NurseUpdate -> event', event.target.name);
     const {
       user: { nurse },
       user,
     } = this.state;
     const { name, checked: value, type } = event.target;
-    console.log('TCL: NurseUpdate -> value', type);
-    console.log('TCL: NurseUpdate -> value', value);
 
     this.setState({
       user: {
@@ -129,12 +119,6 @@ class NurseUpdate extends Component {
   handleFormSubmit = e => {
     e.preventDefault();
     const { username, password, company, user } = this.state;
-    // this.props.handleUpdate({
-    //   username,
-    //   password,
-    //   company,
-    // });
-    console.log('The USER to SEND: ', user);
 
     userService
       .updateUserNurse(user)
@@ -142,7 +126,6 @@ class NurseUpdate extends Component {
         this.setState({
           message: 'User Updated',
         });
-        console.log('UserUpdated');
       })
       .then(() => {
         this.props.userData();
@@ -157,22 +140,11 @@ class NurseUpdate extends Component {
     const {
       user: { nurse },
     } = this.state;
-    console.log('TCL: NurseUpdate -> render -> nurse', nurse);
 
-    // const {
-    //   user: { title, author, description, rating },
-    //   loading,
-    //   message,
-    // } = this.state;
-    console.log('PROPS: ', this.props);
-    // console.log('TCL: Signup -> render -> contractor', contractor);
-    console.log('TCL: Signup -> componentDidMount -> user', user);
-
-    console.log('TCL: NurseUpdate -> render -> nurse.experience', nurse);
     return (
       <div>
         <Title>UPDATE RESUME:</Title>
-        {/* {username} */}
+
         {message && <Message>{message}</Message>}
         {loading && <Spinner src={logo} className="spinner" alt="logo" />}
         {!loading && (
@@ -198,23 +170,6 @@ class NurseUpdate extends Component {
                 onChange={this.handleChangeBox}
               />
 
-              {/* <Label htmlFor="year">NurseDegree (Year):</Label>
-              <Input type="date" name="year" id="year" value={nurse.nurseDegree} onChange={this.handleChange} /> */}
-
-              {/* <Label htmlFor="experience">Experience:</Label>
-              {nurse.resume.experience.map((oneExperience, index) => (
-                <FormExperience
-                  key={index}
-                  nurse={nurse}
-                  company={oneExperience.company}
-                  job={oneExperience.job}
-                  currentJob={oneExperience.currentJob}
-                  startDate={oneExperience.startDate}
-                  endDate={oneExperience.endDate}
-                  jobDescription={oneExperience.jobDescription}
-                  handleChange={this.handleChange}
-                />
-              ))} */}
               <InputSubmit type="submit" value="Update" />
             </form>
           </>
