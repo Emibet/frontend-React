c3165a77063de9653c9bd5ffa7973f98552c4cef
Xavi <xavigallardo@gmail.com>
2019-11-19 01:19:37 +0100

    Add font-family GlobalStyle

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,1 +1,9 @@
-export { default as Button } from './Button';
+import { createGlobalStyle } from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyle = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Tomorrow');
+    font-family: 'Tomorrow', sans-serif;
+  }
+`;
